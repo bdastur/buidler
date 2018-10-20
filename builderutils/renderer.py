0106ad53f0b71b33fdf6df9f401d53703cf1b2cd
Behzad Dastur <bdastur@gmail.com>
2018-10-20 10:12:55 -0700

    Small changes to get end to end flow working.

--- builderutils/renderer.py
+++ builderutils/renderer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 
 
 class Renderer(object):
     def __init__(self):
         pass
 
-    def build_staging_environment(self):
+    def build_staging_environment(self, renderObj):
         ''' Build a new staging path where code gets rendered.
 
         :type  argument:  data type
@@ -15,6 +16,15 @@ class Renderer(object):
 
         :returns:
         '''
+        renderRoot = "/tmp/builder"
+        if not os.path.exists(renderRoot):
+            os.mkdir(renderRoot)
+
+        renderProjectPath = os.path.join(renderRoot, renderObj['app_name'])
+        if not os.path.exists(renderProjectPath):
+            os.mkdir(renderProjectPath)
+
+
     def render_j2_template(self, templateFile, searchPath,
                            renderObj):
         ''' The API will render a Jinja2 template
