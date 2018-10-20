5f777f946b2bcfd89ead5a26a81eb9329d6f0dbf
Behzad Dastur <bdastur@gmail.com>
2018-10-20 07:03:14 -0700

    Minor changes

--- builder/builder.py
+++ builder/builder.py
@@ -2,23 +2,26 @@
 # -*- coding: utf-8 -*-
 
 import click
-
+import builderutils.parser as parser
 
 @click.group()
 def cli():
     pass
 
 @click.command()
-def create():
+@click.option("--configfile", type=click.Path(),
+              help="Builder config", required=True)
+def create(configfile):
     print "create command!"
+    parserobj = parser.BuilderParser(configfile)
 
 
 def main():
     cli.add_command(create)
-    cli() 
+    cli()
 
 if __name__ == "__main__":
-    main() 
+    main()
 
 
 
---
+++ builderutils/renderer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+
+class Renderer(object):
+    def __init__(self):
+        pass
+
+    def build_staging_environment(self):
+        ''' Build a new staging path where code gets rendered.
+
+        :type  argument:  data type
+        :param  argument:  description
+
+        :returns:
+        '''
+    def render_j2_template(self, templateFile, searchPath,
+                           renderObj):
+        ''' The API will render a Jinja2 template
+
+        :type  templateFile:  String
+        :param templateFile: Name of the template file
+
+        :type  searchPath: String
+        :param searchPath: Path to the templates directory
+
+        :type  renderObj:  dict
+        :param renderObj: Dictionary object to substitute the template vars
+
+        :returns:
+        '''
+        pass
+
+
+
+
+
+
