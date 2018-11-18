22d3e7033af63e86efc1e3c678fe9387b522f16a
Behzad Dastur <bdastur@gmail.com>
2018-11-18 05:27:32 -0800

    Additional changes.

--- tests/builder_ut.py
+++ tests/builder_ut.py
@@ -5,13 +5,14 @@
 import unittest
 import yaml
 import builderutils.parser as parser
+import builderutils.renderer as renderer
 
 
 
 class builderUT(unittest.TestCase):
-    def createConfigFile(self):
-        testConfigFile = "/tmp/testdata.yaml"
-        data = {
+    def getConfigObject(self, type='default'):
+        testData = {}
+        testData['default'] = {
             "name": "Test definition",
             "app_type": "flask",
             "app_name": "testapplication",
@@ -21,12 +22,18 @@ class builderUT(unittest.TestCase):
                         "type": "html",
                         "title": "Simple page",
                         "components": {
-                            "basic": "A teest string"
+                            "basic": "A test string"
                         }
                     }
                 }
             }
         }
+        return testData[type]
+
+    def createConfigFile(self):
+        testConfigFile = "/tmp/testdata.yaml"
+
+        data = self.getConfigObject()
         with open(testConfigFile, "w") as outfile:
             yaml.dump(data, outfile, default_flow_style=False)
 
@@ -38,8 +45,32 @@ class builderUT(unittest.TestCase):
     def testParserInitialize(self):
         print "Parser Initialize test"
 
+        # Valid keys
+        dataKeys = ['flask_template', 'user_config', 'html_template']
         # Create a test config file
         configFile = self.createConfigFile()
-        parserObj = parser.BuilderParser(configFile, templateRoot="../templates")
+        parserObj = parser.BuilderParser(configFile,
+                                         templateRoot="../templates")
+        self.assertTrue(len(parserObj.parsedData) == 3)
+        self.assertEqual(parserObj.parsedData.keys(),
+                         dataKeys, msg="Unexpected keys!")
+
+    def testRenderHtml(self):
+        print "Test basic Rendering."
+
+        # Create a test config file
+        configFile = self.createConfigFile()
+
+        # Get ParserObj
+        parserObj = parser.BuilderParser(configFile,
+                                         templateRoot="../templates")
+
+        renderObj = renderer.Renderer()
+        renderObj.build_staging_environment(parserObj.parsedData)
+
+        userConfig = parserObj.parsedData['user_config']
+        htmlTemplate = parserObj.parsedData['html_template']
+
+        renderObj.build_html_documents(htmlTemplate, userConfig)
 
 
