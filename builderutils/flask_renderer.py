aaf91a32a6c0f168fe9660ace3998fac4518b7a2
Behzad Dastur <bdastur@gmail.com>
2018-11-21 15:14:24 -0800

    Refactoring the Flask Renderer class

--- builderutils/flask_renderer.py
+++ builderutils/flask_renderer.py
@@ -13,6 +13,7 @@ class FlaskRenderer(object):
         # Initialize logging
         builderLogger = logger.BuilderLogger(name=__name__)
         self.logger = builderLogger.logger
+        self.renderer = Renderer()
 
         if configFile is None:
             self.logger.error("Config file is None")
@@ -29,12 +30,43 @@ class FlaskRenderer(object):
         # Parse our templates
         self.parserObj = parser.BuilderParser(configFile,
                                               templateRoot=templateRoot)
+        if self.parserObj.initialized is not True:
+            self.logger.error("Could not Parse config or templates!")
+            return
+
+        if self.setupStagingEnvironment() != 0:
+            self.logger.error("Failed to setup Staging Environment!")
+            return
 
         self.initialized = True
 
+    def setupStagingEnvironment(self):
+        ''' Createe a staging environment
+
+        :type  argument:  data type
+        :param  argument:  description
+
+        :returns:
+        '''
+        # Validate input
+        userConfig = self.parserObj.parsedData.get('user_config', None)
+        if userConfig is None:
+            self.logger.error("user_config is Empty!")
+            return -1
+
+        app_type = self.parserObj.parsedData['user_config'].get('app_type', None)
+        if app_type is None:
+            self.logger.error("Could not find app_type in user config!")
+            return -1
+
+        if app_type != "flask":
+            self.logger.error("app_type should be flask")
+            return -1
+
+        return self.__setupStagingDirectories()
 
 
-    def setup_environment(self, renderRoot, renderObj):
+    def __setupStagingDirectories(self):
         ''' Setup Flask Environment
 
         :type  argument:  data type
@@ -42,13 +74,23 @@ class FlaskRenderer(object):
 
         :returns:
         '''
-        renderProjectPath = os.path.join(renderRoot,
-                                         renderObj['user_config']['app_name'])
-        if not os.path.exists(renderProjectPath):
-            os.mkdir(renderProjectPath)
+        renderRoot = "/tmp/builder"
+        if not os.path.exists(renderRoot):
+            try:
+                os.mkdir(renderRoot)
+            except OSError as err:
+                self.logger.error("Failed to create folder %s", renderRoot)
+                return -1
 
-        templatesDir = os.path.join(renderProjectPath, 'templates')
-        staticDir = os.path.join(renderProjectPath, 'static')
+        renderObj = self.parserObj.parsedData
+
+        self.renderProjectPath = os.path.join(renderRoot,
+                                         renderObj['user_config']['app_name'])
+        if not os.path.exists(self.renderProjectPath):
+            os.mkdir(self.renderProjectPath)
+
+        templatesDir = os.path.join(self.renderProjectPath, 'templates')
+        staticDir = os.path.join(self.renderProjectPath, 'static')
         jsDir = os.path.join(staticDir, 'js')
         cssDir = os.path.join(staticDir, 'css')
 
@@ -64,7 +106,62 @@ class FlaskRenderer(object):
         if not os.path.exists(cssDir):
             os.mkdir(cssDir)
 
-        return renderProjectPath
+        return 0
+
+    def renderFlaskApplication(self):
+        ''' Render the Flask application
+        :type  argument:  data type
+        :param  argument:  description
+
+        :returns:
+        '''
+        flaskTemplate = self.parserObj.parsedData['flask_template']
+        renderObj = self.parserObj.parsedData['user_config']
+
+        appInfo = renderObj['components']['app']
+        flaskRoutes = appInfo['routes']
+
+        print "app info: ", appInfo
+        print "flask Routes: ", flaskRoutes
+
+        renderedData = ""
+        # Header
+        flaskTmpl = flaskTemplate['header']
+        renderedData += self.renderer.render_j2_template_string(flaskTmpl,
+                                                       appInfo)
+
+        # Flask Imports
+        flaskTmpl = flaskTemplate['imports']
+        renderedData += self.renderer.render_j2_template_string(flaskTmpl,
+                                                       appInfo)
+
+        # Flask App initialization
+        flaskTmpl = flaskTemplate['app_init']
+        renderedData += self.renderer.render_j2_template_string(flaskTmpl,
+                                                       appInfo)
+
+        # Flask routes
+        for routeName, routeInfo in flaskRoutes.items():
+            print "routename: ", routeName, routeInfo
+            flaskTmpl = flaskTemplate['app_route']
+            renderedData += self.renderer.render_j2_template_string(flaskTmpl,
+                                                       routeInfo)
+
+        # Flask Run
+        flaskTmpl = flaskTemplate['app_run']
+        renderedData += self.renderer.render_j2_template_string(flaskTmpl,
+                                                       appInfo)
+        print renderedData
+
+        # Create a html file
+        fileName = "app.py"
+        print "File name to create: ", fileName
+
+        filePath = os.path.join(self.renderProjectPath, fileName)
+        print "File path: ", filePath
+        with open(filePath, 'w') as fHandle:
+            fHandle.write(renderedData)
+
 
 
 class HTMLRenderer(object):
@@ -76,6 +173,7 @@ class HTMLRenderer(object):
 
     def buildHTMLDocument(self):
         pass
+
 
 class Renderer(object):
     def __init__(self):
