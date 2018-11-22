09e5e55155ec9c8d711eefcbf0febe6a7f5a530e
Behzad Dastur <bdastur@gmail.com>
2018-11-21 16:05:19 -0800

    Cleanup renderers. Move them in separate files

--- builderutils/flask_renderer.py
+++ builderutils/flask_renderer.py
@@ -6,6 +6,9 @@ import shutil
 import jinja2
 import builderutils.logger as logger
 import builderutils.parser as parser
+import builderutils.renderer as renderer
+import builderutils.html_renderer as html_renderer
+
 
 class FlaskRenderer(object):
     def __init__(self, configFile, templateRoot="./templates"):
@@ -116,14 +119,13 @@ class FlaskRenderer(object):
 
         :returns:
         '''
-        self.renderer = Renderer()
+        self.renderer = renderer.Renderer()
 
         userConfig = self.parserObj.parsedData['user_config']
         htmlTemplate = self.parserObj.parsedData['html_template']
 
-        self.htmlRenderer = HTMLRenderer(htmlTemplate,
-                                         userConfig, self.renderProjectPath,
-                                         self.renderRoot)
+        self.htmlRenderer = html_renderer.HTMLRenderer(
+            htmlTemplate, userConfig, self.renderProjectPath, self.renderRoot)
 
         self.renderFlaskPythonApplication()
 
@@ -192,6 +194,23 @@ class HTMLRenderer(object):
         self.renderRoot = renderRoot
         self.renderer = Renderer()
 
+    def renderHtmlComponent(self, componentInfo):
+        ''' Render HTML components
+
+        :type  argument:  data type
+        :param  argument:  description
+
+        :returns:
+        '''
+        renderedComponent = ''
+        if componentInfo['type'] == "string":
+            renderedComponent += "<p>"
+            renderedComponent += componentInfo['data']
+            renderedComponent += "</p>"
+            renderedComponent += "\n"
+
+        return renderedComponent
+
     def buildHTMLDocument(self):
         ''' Build HTML documents
 
@@ -227,7 +246,7 @@ class HTMLRenderer(object):
             # We need to go through and add components here
             for component, componentInfo in htmlInfo['components'].items():
                 print "Component >>> ", component
-                renderedData += self.renderer.renderHtmlComponent(componentInfo)
+                renderedData += self.renderHtmlComponent(componentInfo)
 
             # Body end
             renderedData += "</body>\n"
@@ -271,202 +290,3 @@ class HTMLRenderer(object):
             shutil.copytree(renderObj['static_dir'], staticFilePath)
         except Exception as e:
             print "Directory not copied. ", e
-
-class Renderer(object):
-    def __init__(self):
-        self.projectStagingDir = ""
-
-
-    def render_j2_template(self, templateFile, searchPath,
-                           renderObj):
-        ''' The API will render a Jinja2 template
-
-        :type  templateFile:  String
-        :param templateFile: Name of the template file
-
-        :type  searchPath: String
-        :param searchPath: Path to the templates directory
-
-        :type  renderObj:  dict
-        :param renderObj: Dictionary object to substitute the template vars
-
-        :returns:
-        '''
-        template_loader = jinja2.FileSystemLoader(searchpath=searchPath)
-        env = jinja2.Environment(loader=template_loader,
-                                 trim_blocks=True,
-                                 lstrip_blocks=True)
-        template = env.get_template(templateFile)
-        renderedData = template.render(renderObj)
-
-        return renderedData
-
-    def render_j2_template_string(self, templateString, renderObj):
-        ''' one line description
-
-        :type  argument:  data type
-        :param  argument:  description
-
-        :returns:
-        '''
-        env = jinja2.Environment(loader=jinja2.BaseLoader,
-                                 trim_blocks=True,
-                                 lstrip_blocks=True)
-        template = env.from_string(templateString)
-        renderedData = template.render(renderObj)
-        return renderedData
-
-    def renderHtmlComponent(self, componentInfo):
-        ''' Render HTML components
-
-        :type  argument:  data type
-        :param  argument:  description
-
-        :returns:
-        '''
-        renderedComponent = ''
-        if componentInfo['type'] == "string":
-            renderedComponent += "<p>"
-            renderedComponent += componentInfo['data']
-            renderedComponent += "</p>"
-            renderedComponent += "\n"
-
-        return renderedComponent
-
-    def build_html_documents(self, htmlTemplate, renderObj):
-        ''' one line description
-
-        :type  argument:  data type
-        :param  argument:  description
-
-        :returns:
-        '''
-        htmlComponents = renderObj['components']['html']
-        print "html components: ", htmlComponents
-
-        for viewName, htmlInfo in htmlComponents.items():
-            renderedData = ""
-            # Header
-            headerTemplate = htmlTemplate['header']
-            renderedData += self.render_j2_template_string(headerTemplate,
-                                                            htmlInfo)
-
-            # Head
-            headTemplate = htmlTemplate['head']
-            renderedData += self.render_j2_template_string(headTemplate,
-                                                            htmlInfo)
-
-            # Head End
-            renderedData += "</head>\n"
-
-            # Body
-            bodyTemplate = htmlTemplate['body']
-            print "Body template: ", bodyTemplate
-            renderedData += self.render_j2_template_string(bodyTemplate,
-                                                        htmlInfo)
-
-            # We need to go through and add components here
-            for component, componentInfo in htmlInfo['components'].items():
-                print "Component >>> ", component
-                renderedData += self.renderHtmlComponent(componentInfo)
-
-            # Body end
-            renderedData += "</body>\n"
-
-            # HTML End
-            renderedData += "</html>\n"
-            print "RenderedData: ", renderedData
-
-            # Create a html file
-            print renderObj['components']
-
-            fileName = htmlInfo.get('file_name', viewName + ".html")
-            print "File name to create: ", fileName
-
-            filePath = os.path.join(self.projectStagingDir, "templates")
-            filePath = os.path.join(filePath, fileName)
-            print "File path: ", filePath
-            print "Rendered data: ", renderedData
-
-            with open(filePath, 'w') as fHandle:
-                fHandle.write(renderedData)
-
-            # Copy static resources
-            self.build_static_resources(renderObj)
-
-    def build_static_resources(self, renderObj):
-        ''' Static JS, CSS resource creation
-
-        :type  argument:  data type
-        :param  argument:  description
-
-        :returns:
-        '''
-        staticFilePath = os.path.join(self.projectStagingDir, "static")
-
-
-        # Copy CSS Resources
-        if os.path.exists(staticFilePath):
-            shutil.rmtree(staticFilePath)
-
-        try:
-            shutil.copytree(renderObj['static_dir'], staticFilePath)
-        except Exception as e:
-            print "Directory not copied. ", e
-
-
-
-    def build_flask_app(self, flaskTemplate, renderObj):
-        ''' one line description
-
-        :type  argument:  data type
-        :param  argument:  description
-
-        :returns:
-        '''
-        appInfo = renderObj['components']['app']
-        flaskRoutes = appInfo['routes']
-
-        print "app info: ", appInfo
-        print "flask Routes: ", flaskRoutes
-
-        renderedData = ""
-        # Header
-        flaskTmpl = flaskTemplate['header']
-        renderedData += self.render_j2_template_string(flaskTmpl,
-                                                       appInfo)
-
-        # Flask Imports
-        flaskTmpl = flaskTemplate['imports']
-        renderedData += self.render_j2_template_string(flaskTmpl,
-                                                       appInfo)
-
-        # Flask App initialization
-        flaskTmpl = flaskTemplate['app_init']
-        renderedData += self.render_j2_template_string(flaskTmpl,
-                                                       appInfo)
-
-        # Flask routes
-        for routeName, routeInfo in flaskRoutes.items():
-            print "routename: ", routeName, routeInfo
-            flaskTmpl = flaskTemplate['app_route']
-            renderedData += self.render_j2_template_string(flaskTmpl,
-                                                       routeInfo)
-
-        # Flask Run
-        flaskTmpl = flaskTemplate['app_run']
-        renderedData += self.render_j2_template_string(flaskTmpl,
-                                                       appInfo)
-        print renderedData
-
-        # Create a html file
-        fileName = "app.py"
-        print "File name to create: ", fileName
-
-        filePath = os.path.join(self.projectStagingDir, fileName)
-        print "File path: ", filePath
-        with open(filePath, 'w') as fHandle:
-            fHandle.write(renderedData)
-
-
-
