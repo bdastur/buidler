baf8ae432ddcf90ed2a9873a2be83048a1cd7a3e
Behzad Dastur <bdastur@gmail.com>
2019-05-18 10:49:04 -0700

    black formatter

--- tests/builder_ut.py
+++ tests/builder_ut.py
@@ -9,11 +9,10 @@ import builderutils.renderer as renderer
 import builderutils.flask_renderer as flask_renderer
 
 
-
 class BuilderUT(unittest.TestCase):
-    def getConfigObject(self, type='default'):
+    def getConfigObject(self, type="default"):
         testData = {}
-        testData['default'] = {
+        testData["default"] = {
             "name": "Test definition",
             "app_type": "flask",
             "app_name": "testapplication",
@@ -26,13 +25,10 @@ class BuilderUT(unittest.TestCase):
                             "simpletext": {
                                 "type": "string",
                                 "data": "A test string <b>Bold here</b> \
-                                <code>This is code</code>"
+                                <code>This is code</code>",
                             },
-                            "simplebutton": {
-                                "type": "button",
-                                "name": "submit"
-                            }
-                        }
+                            "simplebutton": {"type": "button", "name": "submit"},
+                        },
                     }
                 },
                 "app": {
@@ -44,11 +40,11 @@ class BuilderUT(unittest.TestCase):
                         "index": {
                             "uri": "/",
                             "methods": ["GET"],
-                            "view_filename": "index.html"
+                            "view_filename": "index.html",
                         }
-                    }
-                }
-            }
+                    },
+                },
+            },
         }
         return testData[type]
 
@@ -68,14 +64,14 @@ class BuilderUT(unittest.TestCase):
         print("Parser Initialize test")
 
         # Valid keys
-        dataKeys = ['flask_template', 'user_config', 'html_template']
+        dataKeys = ["flask_template", "user_config", "html_template"]
         # Create a test config file
         configFile = self.createConfigFile()
-        parserObj = parser.BuilderParser(configFile,
-                                         templateRoot="../templates")
+        parserObj = parser.BuilderParser(configFile, templateRoot="../templates")
         self.assertTrue(len(parserObj.parsedData) == 3)
-        self.assertEqual(parserObj.parsedData.keys(),
-                         dataKeys, msg="Unexpected keys!")
+        self.assertEqual(
+            parserObj.parsedData.keys(), dataKeys,
+            msg="Unexpected keys!")
 
     def testRenderHtml(self):
         print("Test basic Rendering.")
@@ -84,15 +80,14 @@ class BuilderUT(unittest.TestCase):
         configFile = self.createConfigFile()
 
         # Get ParserObj
-        parserObj = parser.BuilderParser(configFile,
-                                         templateRoot="../templates")
+        parserObj = parser.BuilderParser(configFile, templateRoot="../templates")
 
         renderObj = renderer.Renderer()
         renderObj.build_staging_environment(parserObj.parsedData)
 
-        userConfig = parserObj.parsedData['user_config']
-        htmlTemplate = parserObj.parsedData['html_template']
-        flaskTemplate = parserObj.parsedData['flask_template']
+        userConfig = parserObj.parsedData["user_config"]
+        htmlTemplate = parserObj.parsedData["html_template"]
+        flaskTemplate = parserObj.parsedData["flask_template"]
 
         renderObj.build_html_documents(htmlTemplate, userConfig)
         renderObj.build_flask_app(flaskTemplate, userConfig)
@@ -103,8 +98,9 @@ class BuilderUT(unittest.TestCase):
         configFile = self.createConfigFile()
 
         renderObj = renderer.Renderer()
-        flaskRendererObj = flask_renderer.FlaskRenderer(configFile,
-                                                        templateRoot="../templates")
+        flaskRendererObj = flask_renderer.FlaskRenderer(
+            configFile, templateRoot="../templates"
+        )
         self.assertEqual(flaskRendererObj.initialized, True)
 
         userConfig = flaskRendererObj.getUserConfig()
@@ -112,40 +108,41 @@ class BuilderUT(unittest.TestCase):
 
         print("user config: %s" % userConfig)
 
-        htmlComponents = userConfig['components']['html']
+        htmlComponents = userConfig["components"]["html"]
 
         for viewName, htmlInfo in htmlComponents.items():
-            bodyTemplate = htmlTemplate['body']
+            bodyTemplate = htmlTemplate["body"]
             renderedData = renderObj.render_j2_template_string(
                 bodyTemplate, htmlInfo)
             print("Rendered data: ", renderedData)
 
-            scriptsTemplate = htmlTemplate['scripts']
+            scriptsTemplate = htmlTemplate["scripts"]
             renderedData = renderObj.render_j2_template_string(
-                scriptsTemplate, htmlInfo)
+                scriptsTemplate, htmlInfo
+            )
             print("Rendered data: ", renderedData)
-
-
 
     def testNewRenderHtml(self):
         print("Test refactored renderer")
 
         flaskRendererObj = flask_renderer.FlaskRenderer(None)
-        self.assertEqual(flaskRendererObj.initialized, False,
-                         "Expected initializedFalse")
+        self.assertEqual(
+            flaskRendererObj.initialized, False, "Expected initializedFalse"
+        )
 
         # Crete a test config file
         configFile = self.createConfigFile()
         flaskRendererObj = flask_renderer.FlaskRenderer(configFile)
 
-        self.assertEqual(flaskRendererObj.initialized, False,
-                         "Exepcted initialized True")
+        self.assertEqual(
+            flaskRendererObj.initialized, False, "Exepcted initialized True"
+        )
 
-        flaskRendererObj = flask_renderer.FlaskRenderer(configFile,
-                                                        templateRoot="../templates")
+        flaskRendererObj = flask_renderer.FlaskRenderer(
+            configFile, templateRoot="../templates"
+        )
 
-        self.assertEqual(flaskRendererObj.initialized, True,
-                         "Exepcted initialized True")
+        self.assertEqual(
+            flaskRendererObj.initialized, True, "Exepcted initialized True"
+        )
         flaskRendererObj.renderFlaskApplication()
-
-
--- tests/parser_ut.py
+++ tests/parser_ut.py
@@ -13,22 +13,22 @@ class TestDataHelper(object):
         self.testConfigFile = "/tmp/testdata.yaml"
         self.createConfigFile()
 
-    def getConfigObject(self, type='default'):
+    def getConfigObject(self, type="default"):
         testData = {}
-        testData['default'] = {
+        testData["default"] = {
             "name": "Basic Template definition",
             "app_type": "flask",
             "app_name": "simple_application",
             "components": {
                 "box1": {
                     "loc": {"row": 0, "column": 0, "column_size": 12},
-                    "type": "string"
+                    "type": "string",
                 },
                 "box2": {
                     "loc": {"row": 1, "column": 0, "column_size": 12},
-                    "type": "string"
-                }
-            }
+                    "type": "string",
+                },
+            },
         }
         return testData[type]
 
@@ -45,14 +45,13 @@ class ParserUt(unittest.TestCase):
         testData = TestDataHelper()
 
         pobj = parser.ConfigParser(testData.testConfigFile)
-        self.assertTrue(pobj.validated is True,
-                        "Parser utils not initialized successfully!")
+        self.assertTrue(
+            pobj.validated is True, "Parser utils not initialized successfully!"
+        )
         print("Pobj: ", pobj)
 
     def test_yamale_basic(self):
         schema = yamale.make_schema("../configs/schema.yaml")
         data = TestDataHelper()
-        data = yamale.make_data("/tmp/testdata.yaml", parser='ruamel')
+        data = yamale.make_data("/tmp/testdata.yaml", parser="ruamel")
         yamale.validate(schema, data)
-
-
